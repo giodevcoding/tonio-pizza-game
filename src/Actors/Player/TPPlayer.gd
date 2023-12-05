a2813ac1fc5d4bd64b910de242f889f9ec862833
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2023-12-04 23:15:04 -0600

    implement player collision

--- src/Actors/Player/TPPlayer.gd
+++ src/Actors/Player/TPPlayer.gd
@@ -1,22 +1,24 @@
 class_name TPPlayer
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 200
-@export var sprintMultiplier = 1.2
-@export var sprintAnimationMultiplier = 1.2
+@export var sprint_multiplier = 1.4
+@export var sprint_animation_multiplier = 1.2
+@export var attack_cooldown_time = 0.3
 
 var screen_size
-var velocity
 
 enum Direction { LEFT, RIGHT, DOWN, UP }
 var direction: Direction = Direction.DOWN
+
+var attack_timer = 0;
+var is_attacking: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	velocity = Vector2.ZERO
-	# hide()
 	initAnimation()
 
 
@@ -47,35 +49,39 @@ func handleMovement(delta):
 		currentVelocity = currentVelocity.normalized() * speed
 
 		if (Input.is_action_pressed("sprint")):
-			currentVelocity = currentVelocity * sprintMultiplier
-
-	print(velocity)
+			currentVelocity = currentVelocity * sprint_multiplier
 	
 	velocity = currentVelocity
+	
+	var motion = velocity * delta / 2
 
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	move_and_collide(motion)
+	move_and_slide()
+	
 
 
 func handleAnimation():
-	if (velocity.x == 0 && velocity.y == 0): 
+	if velocity.x == 0 && velocity.y == 0: 
 		handleIdleAnimation()	
 
-	if (velocity.x != 0 || velocity.y != 0):
+	if velocity.x != 0 || velocity.y != 0:
 		handleMoveAnimation()
 
 
 func handleDirection():
-	if (velocity.x < 0): 
-		direction = Direction.LEFT
-	if (velocity.x > 0):
-		direction = Direction.RIGHT
+	if (Input.is_action_pressed("move_up") || Input.is_action_pressed("move_down")): 
+		if (Input.is_action_pressed("move_up") && !Input.is_action_pressed("move_down")):
+			direction = Direction.UP
+		if (!Input.is_action_pressed("move_up") && Input.is_action_pressed("move_down")):
+			direction = Direction.DOWN
 
-	# Vertical direction gets priority over horizontal direction
-	if (velocity.y < 0):
-		direction = Direction.UP
-	if (velocity.y > 0):
-		direction = Direction.DOWN
+	# Horizontal direction gets priority over vertical direction
+	if (Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right")): 
+		if (Input.is_action_pressed("move_left") && !Input.is_action_pressed("move_right")):
+			direction = Direction.LEFT
+		if (!Input.is_action_pressed("move_left") && Input.is_action_pressed("move_right")):
+			direction = Direction.RIGHT
+
 
 func handleIdleAnimation():
 	if (direction == Direction.DOWN):
@@ -109,8 +115,6 @@ func handleMoveAnimation():
 		$AnimatedSprite2D.animation = 'run-up'
 
 	if (Input.is_action_pressed("sprint")):
-		$AnimatedSprite2D.speed_scale = sprintAnimationMultiplier
+		$AnimatedSprite2D.speed_scale = sprint_animation_multiplier
 	else:
 		$AnimatedSprite2D.speed_scale = 1
-		
-
